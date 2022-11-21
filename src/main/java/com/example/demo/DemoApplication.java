cf47368efe4167f45e4d38a151ba5cc8bedfc295
JunRain <happis9899@gmail.com>
2022-11-21 19:35:30 +0900

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//해당 클래스가 스프링 부트를 설정하는 클래스임을 의미. 베이스 패키지로 간주.
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
